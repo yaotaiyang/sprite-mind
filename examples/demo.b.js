54a54d09f5fa8079f82c593cfb91eaabe8d4938b
yaotaiyang <yaotaiyang99@gmail.com>
2019-11-29 17:24:07 +0800

    some test for layered

---
+++ examples/demo.b.js
@@ -0,0 +1,37 @@
+import { SpriteMind } from '../src'
+
+let data = {
+  text: '脑图测试',
+  fixed: true,
+  draggable: false,
+  mouseEvents: {
+    contextmenu: function(e) {
+      this.attr('text', '测试')
+    }
+  },
+  attrs: { color: '#fff', bgcolor: '#708fbf', border: null },
+  children: [
+    {
+      text: '二级脑图1',
+      children: [{ text: '三级脑图' }, { text: '三级脑图2' }, { text: '三级脑图3' }, { text: '三级脑图4' }, { text: '三级脑图5' }],
+      expand: function(unfold, data) {
+        console.log(unfold, data)
+      }
+    },
+    { text: '二级脑图12', children: [{ text: '三级脑图' }, { text: '三级脑图2' }, { text: '三级脑图3' }, { text: '三级脑图4' }, { text: '三级脑图5' }] },
+    { text: '二级脑图22222', children: [{ text: '三级脑图', children: [{ text: '三级脑图4' }] }, { text: '三级脑图2' }, { text: '三级脑图3' }] }
+  ]
+}
+let spriteMind = new SpriteMind(
+  {
+    selector: '#app',
+    animation: false,
+    size: [800, 600]
+  },
+  data
+)
+window.oncontextmenu = function(e) {
+  //取消默认的浏器自带右键 很重要！览！
+  e.preventDefault()
+}
+console.log(spriteMind)
--- examples/demo.js
+++ examples/demo.js
@@ -1,37 +1,75 @@
-import { SpriteMind } from '../src'
+import { layered, chebyshev, manhattan, euclidean, orthogonal } from 'auto-layouter'
+import { Node, Link, Stage } from 'sprite-nodes-links'
+import { Polyline } from 'spritejs'
+import filterClone from 'filter-clone'
+let stage = new Stage({
+  selector: '#app',
+  size: [800, 600]
+})
+let node1 = new Node({ pos: [200, 200], fixed: true, text: '不受力固定点' })
+let node2 = new Node({ pos: [200, 200], fixed: true, text: 'y轴受力', forceAxis: 'y' })
+let node3 = new Node({ pos: [200, 200], fixed: true, text: '自由节点1' })
+let node4 = new Node({ pos: [200, 200], fixed: true, text: '自由节点2' })
+let node5 = new Node({ pos: [200, 200], fixed: true, text: '自由节点3' })
 
-let data = {
-  text: '脑图测试',
-  fixed: true,
-  draggable: false,
-  mouseEvents: {
-    contextmenu: function(e) {
-      this.attr('text', '测试')
-    }
-  },
-  attrs: { color: '#fff', bgcolor: '#708fbf', border: null },
-  children: [
-    {
-      text: '二级脑图1',
-      children: [{ text: '三级脑图' }, { text: '三级脑图2' }, { text: '三级脑图3' }, { text: '三级脑图4' }, { text: '三级脑图5' }],
-      expand: function(unfold, data) {
-        console.log(unfold, data)
-      }
-    },
-    { text: '二级脑图12', children: [{ text: '三级脑图' }, { text: '三级脑图2' }, { text: '三级脑图3' }, { text: '三级脑图4' }, { text: '三级脑图5' }] },
-    { text: '二级脑图22222', children: [{ text: '三级脑图', children: [{ text: '三级脑图4' }] }, { text: '三级脑图2' }, { text: '三级脑图3' }] }
-  ]
+let arrLinks = [
+  { startId: node1.attr('id'), endId: node2.attr('id') },
+  { startId: node2.attr('id'), endId: node3.attr('id') },
+  { startId: node3.attr('id'), endId: node5.attr('id') },
+  { startId: node4.attr('id'), endId: node5.attr('id') },
+  { startId: node5.attr('id'), endId: node1.attr('id') }
+]
+stage.append([node1, node2, node3, node4, node5])
+let layer = stage.layers.default
+
+//stage.append([node1, node2, node3, node4])
+stage.checkForceLink(true)
+window.stage = stage
+let nodes = []
+let edges = []
+stage.nodes.forEach(node => {
+  let sizeBox = node.sizeBox
+  let width = sizeBox[2] - sizeBox[0]
+  let height = sizeBox[3] - sizeBox[1]
+  nodes.push({ $dom: node, id: node.__attrs.id, width, height })
+})
+arrLinks.forEach(link => {
+  //数据转换
+  let { startId: v, endId: w } = link
+  let edge = { v, w }
+  edge.$line = new Polyline()
+  layer.append(edge.$line)
+  edges.push(edge)
+})
+const graph = {
+  nodes,
+  edges
 }
-let spriteMind = new SpriteMind(
-  {
-    selector: '#app',
-    animation: true,
-    size: [800, 600]
-  },
-  data
-)
-window.oncontextmenu = function(e) {
-  //取消默认的浏器自带右键 很重要！览！
-  e.preventDefault()
-}
-console.log(spriteMind)
+layered(graph)
+
+let boxs = filterClone(nodes, ['x', 'y', 'width', 'height', 'id'])
+console.log(boxs)
+edges.forEach(edge => {
+  let startNode = nodes.filter(n => n.id === edge.v)[0]
+  let endNode = nodes.filter(n => n.id === edge.w)[0]
+  edge.from = {
+    x: startNode.x,
+    y: startNode.y,
+    direction: 'mid'
+  }
+  edge.to = {
+    x: endNode.x,
+    y: endNode.y,
+    direction: 'mid'
+  }
+  let pBoxs = boxs.filter(box => box.id !== startNode.id && box.id !== endNode.id)
+  pBoxs = filterClone(pBoxs, [], ['id'])
+  edge.points = orthogonal(edge.from, edge.to, pBoxs)
+  edge.$line.attr({ points: edge.points.map(p => [p.x, p.y]) })
+})
+console.log(edges)
+graph.nodes.forEach(node => {
+  node.$dom.attr({ pos: [node.x, node.y] })
+})
+
+console.log(graph)
